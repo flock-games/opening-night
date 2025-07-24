16831bb26907ea1ddf9e8e4044225fc01debfa71
Tyler Dawson <tddawson@gmail.com>
2025-07-23 21:54:43 -0700

    Basic fetching of user's liked trailers

--- convex/myFunctions.ts
+++ convex/myFunctions.ts
@@ -1,81 +1,64 @@
-import { v } from "convex/values";
-import { query, mutation, action } from "./_generated/server";
-import { api } from "./_generated/api";
+import { action } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
+import { createClerkClient } from "@clerk/backend";
 
-// Write your Convex functions in any file inside this directory (`convex`).
-// See https://docs.convex.dev/functions for more.
-
-// You can read data from the database via a query:
-export const listNumbers = query({
-  // Validators for arguments.
-  args: {
-    count: v.number(),
-  },
-
-  // Query implementation.
+export const getYoutubeLikes = action({
   handler: async (ctx, args) => {
-    //// Read the database as many times as you need here.
-    //// See https://docs.convex.dev/database/reading-data.
-    const numbers = await ctx.db
-      .query("numbers")
-      // Ordered by _creationTime, return most recent
-      .order("desc")
-      .take(args.count);
+    const clerkClient = createClerkClient({
+      secretKey: process.env.CLERK_CLIENT_SECRET,
+    });
+
     const userId = await getAuthUserId(ctx);
-    const user = userId === null ? null : await ctx.db.get(userId);
-    return {
-      viewer: user?.email ?? null,
-      numbers: numbers.reverse().map((number) => number.value),
-    };
-  },
-});
+    if (!userId) {
+      return;
+    }
+    const clerkResponse = await clerkClient.users.getUserOauthAccessToken(
+      userId,
+      "google",
+    );
+    const accessToken = clerkResponse.data[0].token;
 
-// You can write data to the database via a mutation:
-export const addNumber = mutation({
-  // Validators for arguments.
-  args: {
-    value: v.number(),
-  },
+    let trailersData: any[] = [];
+    let pageToken = undefined;
+    let page = 1;
+    do {
+      if (page % 10 === 0) {
+        console.log(`Fetching page ${page++} of YouTube likes...`);
+      }
+      let url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&myRating=like&maxResults=50`;
+      if (pageToken) {
+        url += `&pageToken=${pageToken}`;
+      }
+      url += `&key=${process.env.GOOGLE_API_KEY}`;
+      const response = await fetch(url, {
+        headers: new Headers({
+          Authorization: `Bearer ${accessToken}`,
+        }),
+      });
+      const data = await response.json();
 
-  // Mutation implementation.
-  handler: async (ctx, args) => {
-    //// Insert or modify documents in the database here.
-    //// Mutations can also read from the database like queries.
-    //// See https://docs.convex.dev/database/writing-data.
-
-    const id = await ctx.db.insert("numbers", { value: args.value });
-
-    console.log("Added new document with id:", id);
-    // Optionally, return a value from your mutation.
-    // return id;
-  },
-});
-
-// You can fetch data from and send data to third-party APIs via an action:
-export const myAction = action({
-  // Validators for arguments.
-  args: {
-    first: v.number(),
-    second: v.string(),
-  },
-
-  // Action implementation.
-  handler: async (ctx, args) => {
-    //// Use the browser-like `fetch` API to send HTTP requests.
-    //// See https://docs.convex.dev/functions/actions#calling-third-party-apis-and-using-npm-packages.
-    // const response = await ctx.fetch("https://api.thirdpartyservice.com");
-    // const data = await response.json();
-
-    //// Query data by running Convex queries.
-    const data = await ctx.runQuery(api.myFunctions.listNumbers, {
-      count: 10,
-    });
-    console.log(data);
-
-    //// Write data by running Convex mutations.
-    await ctx.runMutation(api.myFunctions.addNumber, {
-      value: args.first,
-    });
+      if ("items" in data) {
+        const pageTrailers = data.items
+          .filter((item: any) =>
+            item.snippet.title.toLowerCase().includes("trailer"),
+          )
+          .map((item: any) => {
+            return {
+              title: item.snippet.title,
+              tags: item.snippet.tags || [],
+              categoryId: item.snippet.categoryId,
+            };
+          });
+        trailersData = trailersData.concat(pageTrailers);
+      }
+      if ("nextPageToken" in data && data.nextPageToken !== pageToken) {
+        pageToken = data.nextPageToken;
+        page++;
+      } else {
+        pageToken = undefined;
+      }
+    } while (pageToken !== undefined && page < 100);
+    console.log("Trailers Data:", trailersData);
+    return trailersData;
   },
 });
