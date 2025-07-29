abe253257f590a40537bc38d1fa47386fec42314
Tyler Dawson <tddawson@gmail.com>
2025-07-28 17:33:58 -0700

    Removes former sign in button.

--- src/SignIn.tsx
+++
@@ -1,8 +0,0 @@
-import { useAuthActions } from "@convex-dev/auth/react";
-
-export function SignIn() {
-  const { signIn } = useAuthActions();
-  return (
-    <button onClick={() => void signIn("google")}>Sign in with Google</button>
-  );
-}
